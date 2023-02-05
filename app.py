998759671597607149ff6a6ebe35debcd37cf9c1
maxd <raydoom@163.com>
2023-02-05 17:07:35 +0800

    .

--- app.py
+++ app.py
@@ -5,16 +5,18 @@ from tkinter import ttk
 from tkinter.filedialog import *
 from utils import cert_parser, crl_parser
 
-file_path = "/Users/maxd/Downloads/1.pem"
-subject_info,subject_info_xq= cert_parser.get_cert_info(file_path)
-issue = crl_parser.get_crl_info()
 
-def selectFile():
+
+#issue = crl_parser.get_crl_info()
+
+def selectFile(a1):
     global img
     file_path = askopenfilename()  # 选择打开什么文件，返回文件名
     print(file_path)
-
+    a1.update()
     return file_path
+
+subject_info,subject_info_xq= cert_parser.get_cert_info(file_path)
 
 app = tkinter.Tk()
 app.title("CertViewer")
@@ -47,7 +49,7 @@ label_title = tkinter.Label(app, text='subje',padx=10, pady=15)
 
 label_xiangqing = tkinter.Label(app, text=subject_info_xq,padx=10, pady=10,bg="white",justify="left")
 
-open_file_button = tkinter.Button(app, text='打开文件', command=selectFile)
+open_file_button = tkinter.Button(app, text='打开文件', command=selectFile(app))
 
 label_title.grid(padx=20,)
 tree.grid(padx=20,)
