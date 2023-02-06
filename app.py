f2fa284bc1444d2acbef56b577ecf06aa2cfbc4f
maxd <maxd0801@outlook.com>
2023-02-06 17:19:10 +0800

    .

--- app.py
+++ app.py
@@ -3,51 +3,67 @@
 import tkinter
 from tkinter import ttk
 from tkinter.filedialog import *
+
 from utils import cert_parser, crl_parser
 
-#issue = crl_parser.get_crl_info()
+
+# issue = crl_parser.get_crl_info()
 
 def selectFile():
     file_path = askopenfilename()  # 选择打开什么文件，返回文件名
     print(file_path)
     return file_path
 
-def file_load(tree,label_xiangqing,label_title):
+
+def file_load(tree, label_xiangqing, label_title):
     x = tree.get_children()
     for item in x:
         tree.delete(item)
     file_path = selectFile()
-    cn,base_info_jl,base_info_xq = cert_parser.get_cert_info(file_path)
+    global cn, base_info_jl, base_info_xq
+    cn, base_info_jl, base_info_xq = cert_parser.get_cert_info(file_path)
     label_title.configure(text=cn)
-    tree.insert("", 0, values=('Subject', base_info_jl['subject_info']))
-    tree.insert("", 0, values=('signature_algorithm', base_info_jl['signature_algorithm']))
-    label_xiangqing.configure(text=base_info_xq['subject_info'])
-    print("f1")
+    for v in base_info_jl:
+        tree.insert("", 0, values=(v, base_info_jl[v]))
+    label_xiangqing.configure(text=base_info_xq['subject'])
+    return 0
 
-file_path = "/Users/maxd/Downloads/1.pem"
+
+def select_tree(event, label_xiangqing):
+    for item in tree.selection():
+        item_text = tree.item(item, "values")
+        label_xiangqing.configure(text=base_info_xq[item_text[0]])
+        print(label_xiangqing)
+
+
+def select_adapt(fun, **kwds):
+    return lambda event, fun=fun, kwds=kwds: fun(event, **kwds)
+
+
+# file_path = "/Users/maxd/Downloads/1.pem"
 
 app = tkinter.Tk()
 app.title("CertViewer")
-cn,base_info_jl,base_info_xq = cert_parser.get_cert_info(file_path)
-tree = ttk.Treeview(master=app, show="headings")
-tree["columns"] = ("项目", "值")
-tree.column("项目",width=100)
-tree.column("值",width=260)
-tree.heading("项目", text="项目")
+app.geometry('450x600')
+app.resizable(0, 0)
+tree = ttk.Treeview(master=app, show="headings", height=12)
+tree["columns"] = ("字段", "值")
+tree.column("字段", width=150)
+tree.column("值", width=260)
+tree.heading("字段", text="字段")
 tree.heading("值", text="值")
-tree.insert("", 0, values=('Subject',base_info_jl['subject_info']))
-tree.insert("", 0, values=('signature_algorithm', base_info_jl['signature_algorithm']))
 
-label_title = tkinter.Label(app, text=cn,padx=10, pady=15)
+label_title = tkinter.Label(app, text='', padx=10, pady=15)
+label_xiangqing = tkinter.Label(app, text='', padx=10, pady=10, width=43, height=10, bg="white", justify="left",
+                                anchor='nw', wraplength='400')
 
-label_xiangqing = tkinter.Label(app, text=base_info_xq['subject_info'],padx=10, pady=10,bg="white",justify="left")
+label_title.grid(padx=20, )
+tree.grid(padx=20, )
+label_xiangqing.grid(padx=20, sticky="w", pady=20)
 
-label_title.grid(padx=20,)
-tree.grid(padx=20,)
-label_xiangqing.grid(padx=20, sticky="w",pady=20)
-
-open_file_button = tkinter.Button(app, text='打开文件', command=lambda:file_load(tree,label_xiangqing,label_title))
+open_file_button = tkinter.Button(app, text='打开文件', command=lambda: file_load(tree, label_xiangqing, label_title))
 open_file_button.grid(padx=5, pady=5)
+tree.bind('<<TreeviewSelect>>', select_adapt(select_tree, label_xiangqing=label_xiangqing))
+
 # 进入消息循环
 app.mainloop()
-
--- utils/cert_parser.py
+++ utils/cert_parser.py
@@ -1,35 +1,31 @@
 import OpenSSL
+import time
+from dateutil import parser
 
 def get_cert_info(file_path):
     base_info_jl = {}
     base_info_xq = {}
-    subject_info_list = []
+
     base_info = {}
     extension_info = {}
-    subject_info_jl = ""
-    subject_info_xq = ""
-    # with open("/Users/maxd/Downloads/1.pem", "r") as f:
-    with open(file_path, "r") as f:
+
+
+    with open("/Users/maxd/Downloads/1.pem", "r") as f:
+    # with open(file_path, "r") as f:
         cert_data = f.read()
 
     cert = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, cert_data)
-    certIssue = cert.get_issuer()
-    print(certIssue.commonName)
-    print(cert.get_signature_algorithm().decode("UTF-8"))
+    cn = cert.get_subject().CN
 
-    print(cert.get_extension(1))
-
-    base_info['cn'] = cert.get_subject().CN
-    base_info['o'] = cert.get_subject().O
+    # 主题
     subject_list = list(cert.get_subject().get_components())
     subject_len = len(subject_list)
     print(subject_len)
-    cn = cert.get_subject().CN
+    subject_info_list = []
+    subject_info_jl = ""
+    subject_info_xq = ""
     i = 0
     for i in range(0, subject_len):
-        print (cert.get_subject().get_components()[i][0].decode("UTF-8"),
-               cert.get_subject().get_components()[i][1].decode("UTF-8"))
-
         subject_info_list.append(cert.get_subject().get_components()[i][0].decode("UTF-8")+' = ' \
             +cert.get_subject().get_components()[i][1].decode("UTF-8"))
         subject_info_jl = subject_info_jl + subject_info_list[i]+ ", "
@@ -39,15 +35,72 @@ def get_cert_info(file_path):
     subject_info_xq = subject_info_xq + \
     "                                                                                              "
     print(subject_info_jl)
-    base_info_jl['subject_info'] = subject_info_jl
-    base_info_xq['subject_info'] = subject_info_xq
+    base_info_jl['subject'] = subject_info_jl
+    base_info_xq['subject'] = subject_info_xq
+
+    # 颁发者
+    issuer_list = list(cert.get_issuer().get_components())
+    issuer_len = len(issuer_list)
+    print(issuer_len)
+    issuer_info_list = []
+    issuer_info_jl = ""
+    issuer_info_xq = ""
+    i = 0
+    for i in range(0, issuer_len):
+        issuer_info_list.append(cert.get_issuer().get_components()[i][0].decode("UTF-8")+' = ' \
+            +cert.get_issuer().get_components()[i][1].decode("UTF-8"))
+        issuer_info_jl = issuer_info_jl + issuer_info_list[i]+ ", "
+        issuer_info_xq = issuer_info_xq + issuer_info_list[i]+ "\n"
+        i = i + 1
+    # 控制label宽度
+    issuer_info_xq = issuer_info_xq + \
+    "                                                                                              "
+    print(subject_info_jl)
+    base_info_jl['issuer'] = issuer_info_jl
+    base_info_xq['issuer'] = issuer_info_xq
+
     # 算法
     signature_algorithm = cert.get_signature_algorithm().decode("UTF-8")
-    print(signature_algorithm)
-    signature_algorithm_jl = signature_algorithm
-    signature_algorithm_xq = signature_algorithm
+    base_info_jl['signature_algorithm'] = signature_algorithm
+    base_info_xq['signature_algorithm'] = signature_algorithm
 
-    base_info_jl['signature_algorithm'] = signature_algorithm_jl
-    base_info_xq['signature_algorithm'] = signature_algorithm_xq
+    # 版本version
+    version = cert.get_version()+1
+    base_info_jl['version'] = version
+    base_info_xq['version'] = version
+
+    # 序列号
+    serial_number= hex(cert.get_serial_number())
+    base_info_jl['serial_number'] = serial_number
+    base_info_xq['serial_number'] = serial_number
+
+    #
+    print(cert.get_issuer())
+
+    # 有效期结束
+    not_after = parser.parse(cert.get_notAfter().decode("UTF-8")).strftime('%Y-%m-%d %H:%M:%S')
+    base_info_jl['not_after'] = not_after
+    base_info_xq['not_after'] = not_after
+
+    # 有效期开始
+    not_before = parser.parse(cert.get_notBefore().decode("UTF-8")).strftime('%Y-%m-%d %H:%M:%S')
+    base_info_jl['not_before'] = not_before
+    base_info_xq['not_before'] = not_before
+
+    # 公钥
+    print(cert.get_pubkey().bits())
+    print(OpenSSL.crypto.dump_publickey(OpenSSL.crypto.FILETYPE_PEM, cert.get_pubkey()).decode("utf-8"))
+    pubkey_jl = cert.get_pubkey().bits()
+    pubkey_xq = OpenSSL.crypto.dump_publickey(OpenSSL.crypto.FILETYPE_PEM, cert.get_pubkey()).decode("utf-8")
+    base_info_jl['pubkey'] = pubkey_jl
+    base_info_xq['pubkey'] = pubkey_xq
+
+    # 扩展
+    print(cert.get_extension_count())
+    print(cert.get_extension(2).get_data())
+    for i in range(0, cert.get_extension_count()):
+        print(cert.get_extension(i).__str__())
+        print(cert.get_extension(i).get_short_name())
+        print(cert.get_extension(i))
 
     return cn,base_info_jl,base_info_xq
